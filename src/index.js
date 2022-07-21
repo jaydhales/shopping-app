b119cd3b2c53479c93bd969ffeefc4b6b6d7c2ff
James Osunkiyesi <jaydhales@gmail.com>
2022-07-21 21:47:12 +0100

    Workbox config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,3 +12,6 @@ root.render(
     <App />
   </StrictMode>
 );
+
+
+serviceWorkerRegistration.register();
